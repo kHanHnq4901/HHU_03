984267e2070567df220fd95abf4b93687554b671
nquockhanh4901@gmail.com <nquockhanh4901@gmail.com>
2025-07-07 10:05:42 +0700

    coding map screen

--- android/app/src/main/java/com/hhu_hawaco/MainApplication.kt
+++ android/app/src/main/java/com/hhu_hawaco/MainApplication.kt
@@ -1,5 +1,6 @@
 package com.hhu_hawaco
 
+import com.oblador.vectoricons.VectorIconsPackage;
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
@@ -11,7 +12,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-
+import io.xogus.reactnative.versioncheck.RNVersionCheckPackage
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost =
@@ -20,6 +21,8 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
+               add (VectorIconsPackage())
+               add (RNVersionCheckPackage())
             }
 
         override fun getJSMainModuleName(): String = "index"
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,18 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+// metro.config.js
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+// Bước 1: Lấy config mặc định
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Bước 2: Tùy chỉnh thêm nếu cần
+const customConfig = {
+  // Bạn có thể thêm config tùy chỉnh tại đây
+};
+
+// Bước 3: Gộp và bọc với reanimated
+const mergedConfig = mergeConfig(defaultConfig, customConfig);
+
+// Bước 4: Export
+module.exports = wrapWithReanimatedMetroConfig(mergedConfig);
